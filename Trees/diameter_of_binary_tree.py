2146808c0877e9e7c54b11941c62ca6006b8b010
Gary Smith <garysmith193358@gmail.com>
2023-11-08 14:42:26 -0500

    completed diameter of binary tree

--- Trees/diameter_of_binary_tree.py
+++ Trees/diameter_of_binary_tree.py
@@ -1,25 +1,14 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        #need to implement logic to get the max count of edges on each side, and add the edges of each side to get the answer
-        # recurision 
-        # helper function to recursively handle count of each side
+        res = 0 
 
-        left = self.depth(root.left , 1)
-        right = self.depth(root.right, 1)
+        def dfs(root):
+            if not root: return 0
+            nonlocal res
 
-        print(left, right)
-
-        return left + right
-    
-    def depth(self, root: Optional[TreeNode], count: int ) -> int:
-        # None value
-        if root == None: 
-            return count - 1
+            left, right = dfs(root.left), dfs(root.right)
+            res = max(res, left + right)
+            return 1 + max(left,right)
         
-        left = self.depth(root.left, count + 1)
-        right = self.depth(root.right, count + 1)
-        
-        count = max(left, right)
-        
-        return count
-
+        dfs(root)
+        return res
