291053b0c82c5fd3be50c63fdc77f24d07ab637a
unknown <garysmith193358@gmail.com>
2023-11-26 09:26:44 -0500

    revisited daily temperatures

--- Stack/daily_temperature.py
+++ Stack/daily_temperature.py
@@ -1,18 +1,20 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        res = [0] * len(temperatures)
-        stack = [] # pair [temp, index]
+      answers = [0] * len(temperatures)
+      stack = []
 
-        for i, t in enumerate(temperatures):
-          # if the temperature of the current number is higher 
-          #than the temp at the top of our stack
-          while stack and t > stack[-1][0]:
-            stackTemp, stackIdx = stack.pop()
-            # we get the index and set its position in the results to be 
-            # the difference between the current index and its index 
-            # to represent how many days it has been since a higher temp than that day.
-            res[stackIdx] = i - stackIdx
+      for idx, val in enumerate(temperatures):
+        while stack:
+          stackIdx, stackTemp = stack[-1]
+          if val > stackTemp:
+            daysSince = idx - stackIdx
+            answers[stackIdx] = daysSince
+            stack.pop()
+          
+          else:
+            break
+        
+        stack.append((idx, val))
+      return answers
 
-          stack.append([t, i])
-        return res
-    #Time O(N) #Space O(N)
+      #Time O(N) #Space O(N)
