4bd5048668bee78fc4210bd9ef6d295d37120268
unknown <garysmith193358@gmail.com>
2023-12-04 18:25:09 -0500

    revisited linked list cycle

--- LinkedList/linked_list_cycle.py
+++ LinkedList/linked_list_cycle.py
@@ -1,18 +1,15 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        # using a set to keep track of the nodes that we have visited.
-        # when we are traversing through the nodes, we can check if it is in our set.
-        # if it is, its a cycle.
-        # add each node to set
-        # if we break out the loop without reaching a cycle return false
+      slow = head
+      fast = head
 
-        visited = set()
+      while fast and fast.next:
+        slow = slow.next 
+        fast = fast.next.next
 
-        while head is not None:
-          if head in visited: return True
-          visited.add(head)
-          head = head.next
-        
-        return False
+        if slow == fast:
+          return True
 
-        # Time O(N) Space O(N) 
+      return False
+
+      # Time O(N) #Space O(1)
