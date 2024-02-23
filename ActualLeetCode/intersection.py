bec3947298d01d1cd38b0c059783833707f93f72
Gary Smith <garysmith193358@gmail.com>
2024-02-23 14:57:12 -0500

    added hashmap solution

--- ActualLeetCode/intersection.py
+++ ActualLeetCode/intersection.py
@@ -1,13 +1,31 @@
+# class Solution:
+#     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#         num1Set = set(nums1)
+#         res = []
+
+#         for num in nums2:
+#             if num in num1Set and num not in res:
+#                 res.append(num)
+        
+#         return res
+
+#         # Time O(n + m)
+#         # Space O(n + min(n,m))
+  
+
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        num1Set = set(nums1)
+        count = {}
         res = []
 
-        for num in nums2:
-            if num in num1Set and num not in res:
-                res.append(num)
+        for num in nums1:
+            count[num] = 1
         
+        for num in nums2:
+            if num in count and num not in res:
+                res.append(num)
+
         return res
 
-        # Time O(n + m)
-        # Space O(n + min(n,m))
+        # Time O(N + M)
+        # Space O(N, min(n, m))
