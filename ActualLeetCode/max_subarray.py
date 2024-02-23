808b7fe4762e6d23311970be1f5d928b8e6df57e
Gary Smith <garysmith193358@gmail.com>
2024-02-23 17:30:54 -0500

    added optimized solution for maxSubArray

--- ActualLeetCode/max_subarray.py
+++ ActualLeetCode/max_subarray.py
@@ -1,11 +1,26 @@
-class Solution: #Brute Force
-    def maxSubArray(self, nums: List[int]) -> int:
-        maxSum = nums[0]
-        for i in range(0, len(nums)):
-            curSum = 0
-            for j in range(i, len(nums)):
-                curSum += nums[j]
-                maxSum = max(maxSum, curSum)
+# class Solution: #Brute Force
+#     def maxSubArray(self, nums: List[int]) -> int:
+#         maxSum = nums[0]
+#         for i in range(0, len(nums)):
+#             curSum = 0
+#             for j in range(i, len(nums)):
+#                 curSum += nums[j]
+#                 maxSum = max(maxSum, curSum)
         
-        return maxSum
-        #Time O(n2) # Space O(1)
+#         return maxSum
+#         #Time O(n2) # Space O(1)
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSub = nums[0]
+        curSum = 0
+
+        for num in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += num
+            maxSub = max(maxSub, curSum)
+        
+        return maxSub
+
+    #Time O(N) Space O(1)
